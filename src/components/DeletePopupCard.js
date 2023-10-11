07df81ffb6490474537d3338162f84507165738e
maikoncorrea <maikonacorrea@gmail.com>
2023-10-11 00:57:09 -0300

    created DeletePopupCard.js component

---
+++ src/components/DeletePopupCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function DeletePopupCard() {
+
+    return(
+        <form className="delete" name="delete" novalidate>
+        <div className="delete__popup">
+          <h2 className="delete__popup-title">Tem certeza?</h2>
+          <button name="button" className="delete__button-save" id="delete-button" type="submit">
+            <span className="loading-button-text">Sim</span>
+            <span className="loading-container">
+              <span className="loading-text">Salvando...</span>
+              <span className="loading-animation"></span>
+            </span>
+          </button>
+          <button className="delete__button-close-popup button-close-popup"></button>
+        </div>
+      </form>
+    )
+}
